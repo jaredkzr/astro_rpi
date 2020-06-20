9d6a11d49996f1a1232193bf566ad1fec5130b33
Jared Kizer <jaredkzr@gmail.com>
2020-06-19 20:22:32 -0500

    Update astro_rpi.py
    Replaces single button control with full keyboard input support
    
    Adds preview toggle, burst photo capture, and 30s video capture capabilities

--- astro_rpi.py
+++ astro_rpi.py
@@ -3,12 +3,15 @@ from datetime import datetime
 import time
 from signal import pause
 from picamera import PiCamera
+from os import system
 
 indicator = LED(17)
-button = Button(15)
+#button = Button(15)
 camera = PiCamera()
 camera.resolution = (4056, 3040)
-previewRes = (1280, 720)
+previewRes = (1024, 768)
+run = True
+preview_on = False
 
 def capture_image():
     indicator.on()
@@ -16,21 +19,54 @@ def capture_image():
     filename = "/home/pi/piCam/capture-%02d%02d%02d%04d.jpg" % (timenow.hour, timenow.minute, timenow.second, timenow.microsecond)
     camera.capture("%s" %filename)
     print("Captured %s" % filename)
-    time.sleep(0.1)
+    #time.sleep(0.1)
     indicator.off()
-    camera.start_preview(resolution=previewRes)
 # add folder creation if doesn't exist
 
-def exit_handler():
-    for x in range(0,3):
-        indicator.on()
-        time.sleep(0.1)
-        indicator.off()
-        time.sleep(0.1)
-    camera.stop_preview()
+def capture_vid():
+    indicator.on()
+    timenow = datetime.now()
+    filename = "/home/pi/piCam/capture-%02d%02d%02d%04d.h264" % (timenow.hour, timenow.minute, timenow.second, timenow.microsecond)
+    camera.start_recording("%s" %filename)
+    camera.wait_recording(30)
+    camera.stop_recording
+    print("Captured %s" % filename)
+    #time.sleep(0.1)
+    indicator.off()
     
-button.when_held = exit_handler
-button.when_pressed = capture_image
-camera.start_preview(resolution=previewRes)
+while run == True:
+    print('Options')
+    print('1    Toggle preview')
+    print('2    Single image capture')
+    print('3    10x burst image capture')
+    print('4    30s video capture')
+    print('0    Exit')
+    print('\n')
+    input_var = int(input('Enter an option: '))
 
-pause()
+    if input_var == 1:
+        if preview_on == False:
+            camera.start_preview(resolution=previewRes)
+            preview_on = True
+        else:
+            camera.stop_preview()
+            preview_on = False
+    elif input_var == 2:
+        capture_image()
+    elif input_var == 3:
+        for x in range(0,9):
+            capture_image()
+    elif input_var == 4:
+        print('Video recording begin...')
+        camera.resolution = (1920, 1080)
+        capture_vid()
+        print('Video recording end')
+    elif input_var == 0:
+        run = False
+        exit
+    else:
+        #system('clear')
+        print(' ')
+        print('=============')
+        print('Invalid input')
+        print('=============')
